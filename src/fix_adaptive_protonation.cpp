df03d530cbbb9dc7b8f025683c17657dea2fd29f
Mahdi Tavakol <mahditavakol90@gmail.com>
2025-10-17 10:18:00 +0100

    Reduced the ramp slope in the charge change during the conversion from solvent to solid in the fix_adaptive_protonation.cpp

--- src/fix_adaptive_protonation.cpp
+++ src/fix_adaptive_protonation.cpp
@@ -143,7 +143,7 @@ FixAdaptiveProtonation::FixAdaptiveProtonation(LAMMPS *lmp, int narg, char **arg
     */
   if (!(flags & INIT_MID)) n_protonable = 0;
 
-  nRampStep = 10000;
+  nRampStep = 1000000;
 }
 
 /* --------------------------------------------------------------------------------------- */
