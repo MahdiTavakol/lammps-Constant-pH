4b602ef945ca824c89aa2ad01024a67e05e696c3
Mahdi Tavakol <mahditavakol90@gmail.com>
2026-01-13 12:58:54 +0000

    Added vdwl to the compute_epair and restored the full environment coupling

--- src/CONSTANT_PH/fix_constant_pH.cpp
+++ src/CONSTANT_PH/fix_constant_pH.cpp
@@ -75,7 +75,7 @@ static constexpr double tol = 1e-5;
 static constexpr double max_lambda_buff_0 = 1.05;
 static constexpr double min_lambda = -0.1;
 static constexpr double max_lambda = 1.1;
-static constexpr double environment_coupling = 0.0;
+static constexpr double environment_coupling = 1.0;
 
 /* ---------------------------------------------------------------------- */
 
@@ -1867,7 +1867,7 @@ double FixConstantPH::compute_epair()
 
   double one = 0.0;
   double energy;
-  if (force->pair) one += force->pair->eng_coul;
+  if (force->pair) one += force->pair + force->pair->eng_coul;
   
 
 
