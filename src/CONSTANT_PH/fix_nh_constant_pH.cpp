5f43591ee79816d68018b489ec87246752b76a4b
Mahdi Tavakol <mahditavakol90@gmail.com>
2026-01-15 15:55:48 +0000

    Changed the checkoutofbounds algorithm

--- src/CONSTANT_PH/fix_nh_constant_pH.cpp
+++ src/CONSTANT_PH/fix_nh_constant_pH.cpp
@@ -249,9 +249,9 @@ void FixNHConstantPH::nh_v_temp()
   auto checkOutBounds = [&](void)
   {
     for (int i = 0; i < n_lambdas; i++) {
-      if (x_lambdas[i][0] < -0.1 || x_lambdas[i][0] > 1.1) {
+      if (x_lambdas[i][0] < -0.05 || x_lambdas[i][0] > 1.05) {
         v_lambdas[i][0] = std::abs(v_lambdas[i][0]);
-        if (x_lambdas[i][0] > 1.1)
+        if (x_lambdas[i][0] > 1.05)
           v_lambdas[i][0] = -v_lambdas[i][0];  
       }
        v_lambdas[i][0] = -static_cast<int>((x_lambdas[i][0]>0)-(x_lambdas[i][0]<0))*std::abs(v_lambdas[i][0]);
@@ -265,9 +265,9 @@ void FixNHConstantPH::nh_v_temp()
     }
 
     if (lambda_integration_flags & BUFFER) {
-       if (x_lambda_buff < -0.1 || x_lambda_buff > 1.1) {
+       if (x_lambda_buff < -0.05 || x_lambda_buff > 1.05) {
          v_lambda_buff = std::abs(v_lambda_buff);
-         if (x_lambda_buff > 1.1)
+         if (x_lambda_buff > 1.05)
            v_lambda_buff = -v_lambda_buff;
        }
     }
