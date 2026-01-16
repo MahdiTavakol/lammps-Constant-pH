646d5f71fe2671fcb9ca8b65bc802aa4bf9042e4
Mahdi Tavakol <mahditavakol90@gmail.com>
2026-01-16 14:35:32 +0000

    Some minor changed

--- src/CONSTANT_PH/fix_constant_pH.cpp
+++ src/CONSTANT_PH/fix_constant_pH.cpp
@@ -674,7 +674,7 @@ void FixConstantPH::update_a_lambda()
 
     // I am not sure about the sign of the f*kT*log(10)*(pK-pH)
     this->H_lambdas[i] = environment_coupling*kcal2kj*(lambdas[i][0]*HAs[i] + (1.0-lambdas[i][0])*HBs[i]) -fs[i] * kT * log(10) * (pK - pH) + kj2kcal * Us[i] +
-        (m_lambdas[i][0] / 2.0) * (v_lambdas[i][0] * v_lambdas[i][0]) * (force->mvv2e);    
+        //(m_lambdas[i][0] / 2.0) * (v_lambdas[i][0] * v_lambdas[i][0]) * (force->mvv2e);    
       // This might not be needed. May be I need to tally this into energies.
     // I might need to use the leap-frog integrator and so this function might need to be in other functions than postforce()
   }
