a592900ab5427c52d1c0d948086035b893f0dec5
Mahdi Tavakol <mahditavakol90@gmail.com>
2026-01-16 15:22:47 +0000

    Some minor modifications

--- src/CONSTANT_PH/fix_constant_pH.cpp
+++ src/CONSTANT_PH/fix_constant_pH.cpp
@@ -680,11 +680,11 @@ void FixConstantPH::update_a_lambda()
   }
 
   if (flags & BUFFER) {
-    double f_lambda_buff = -(kj2kcal * dU_buff);
+    double f_lambda_buff = -0.0*(kj2kcal * dU_buff);
     a_lambda_buff = aUnit * 
         f_lambda_buff / m_lambda_buff;    // the fix_nh_constant_pH itself takes care of units
     this->H_lambda_buff = 
-        kj2kcal * U_buff + N_buff * (m_lambda_buff / 2.0) * (v_lambda_buff * v_lambda_buff) * (force->mvv2e);
+        0.0*kj2kcal * U_buff + 0.0*N_buff * (m_lambda_buff / 2.0) * (v_lambda_buff * v_lambda_buff) * (force->mvv2e);
   }
 }
 
