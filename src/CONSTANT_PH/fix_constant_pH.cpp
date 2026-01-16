bbe7514bfdd5f4b3375ecd46d70793afcbd5429b
Mahdi Tavakol <mahditavakol90@gmail.com>
2026-01-16 14:26:09 +0000

    Increased the w value in the calculate_dUs file to 1000 and removed the boundout boundary

--- src/CONSTANT_PH/fix_constant_pH.cpp
+++ src/CONSTANT_PH/fix_constant_pH.cpp
@@ -75,7 +75,7 @@ static constexpr double tol = 1e-5;
 static constexpr double max_lambda_buff_0 = 1.05;
 static constexpr double min_lambda = -0.1;
 static constexpr double max_lambda = 1.1;
-static constexpr double environment_coupling = 0.0;
+static constexpr double environment_coupling = 1.0;
 
 /* ---------------------------------------------------------------------- */
 
@@ -298,7 +298,7 @@ int FixConstantPH::setmask()
 void FixConstantPH::init()
 {
   // default values from Donnini, Ullmann, J Chem Theory Comput 2016 - Table S2
-  w = 50; //200;
+  w = 200; //200;
   s = 0.3;//0.3;
   h = 10; //7.0;
   k = 6.267; //4.417;       //2.553;
