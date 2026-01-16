cb9a2a6f24c18331fabeec003c0f7bb9ba212d22
Mahdi Tavakol <mahditavakol90@gmail.com>
2026-01-16 16:19:18 +0000

    Removed the Bussi thermostat

--- src/CONSTANT_PH/fix_nh_constant_pH.cpp
+++ src/CONSTANT_PH/fix_nh_constant_pH.cpp
@@ -353,9 +353,9 @@ void FixNHConstantPH::nh_v_temp()
 
        // first, the lambdas
        for (int i = 0; i < n_lambdas; i++) {
-          v_lambdas[i][0] *= alpha_bussi1;
+          //v_lambdas[i][0] *= alpha_bussi1;
           for (int j = 1; j < 3; j++) {
-            v_lambdas[i][j] *= alpha_bussi2;
+            //v_lambdas[i][j] *= alpha_bussi2;
           }
        }
        // and then the buffer 
