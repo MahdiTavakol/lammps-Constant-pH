38bd3338a8ac02a67b42c0c8ac89bbe68408e003
Mahdi Tavakol <mahditavakol90@gmail.com>
2026-01-22 11:51:43 +0000

    Revert "Another test patch"
    This reverts commit ec53141aa965c466dc216f819de78d640787eff0.

--- src/CONSTANT_PH/fix_constant_pH.h
+++ src/CONSTANT_PH/fix_constant_pH.h
@@ -30,6 +30,9 @@ FixStyle(constant_pH,FixConstantPH);
 
 #include "fix.h"
 #include "fix_adaptive_protonation.h"
+#include "neigh_list.h"
+#include "neighbor.h"
+#include "neigh_request.h"
 #include "pair.h"
 
 namespace LAMMPS_NS {
@@ -54,6 +57,8 @@ class FixConstantPH : public Fix {
   void copy_arrays(int, int, int) override;
   int pack_exchange(int i, double *buf) override;
   int unpack_exchange(int nlocal, double *buf) override;
+
+  void init_list(int, class NeighList *) override;
 
  protected:
   int flags;
@@ -108,6 +113,9 @@ class FixConstantPH : public Fix {
   std::unique_ptr<double []> H_lambdas;
   double H_lambda_buff;
   double H_lambda_prev;
+
+  // Neighbour list (used to calculate the interpolation)
+  class NeighList* list;
   
 
   // lambda temperature
