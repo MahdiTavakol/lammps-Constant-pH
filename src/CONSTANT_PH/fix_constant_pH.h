66140f2adcd8d75b34cb7e439c8e45735e124813
Mahdi Tavakol <mahditavakol90@gmail.com>
2026-01-22 11:52:09 +0000

    Revert "Another test patch"
    This reverts commit b3e4a30367f946db2b3d0cbc1ddfd16959372cdd.

--- src/CONSTANT_PH/fix_constant_pH.h
+++ src/CONSTANT_PH/fix_constant_pH.h
@@ -115,7 +115,7 @@ class FixConstantPH : public Fix {
   double H_lambda_prev;
 
   // Neighbour list (used to calculate the interpolation)
-  class NeighList* list;
+  NeighList* list;
   
 
   // lambda temperature
