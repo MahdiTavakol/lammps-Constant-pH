77a31864b3c8186fa7fd504a7275c43a07d6bcff
Mahdi Tavakol <mahditavakol90@gmail.com>
2026-01-27 11:05:47 +0000

    reduced the m_buff

--- src/CONSTANT_PH/fix_constant_pH.cpp
+++ src/CONSTANT_PH/fix_constant_pH.cpp
@@ -318,7 +318,7 @@ void FixConstantPH::init()
   a_buff = 0.04764;
   b_buff = -0.09706;
   r_buff = 16.458;
-  m_buff = 0.70; //0.1507; Increased the m so that there is no force on the lambda buff at 1.0
+  m_buff = 0.20; //0.1507; Increased the m so that there is no force on the lambda buff at 1.0
   d_buff = 0.0;
 
   // Reading the pH structure files
