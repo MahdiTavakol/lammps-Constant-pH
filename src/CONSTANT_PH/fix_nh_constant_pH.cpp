59dd958432fcfdac7dda29f7fd1c625d76ced5a8
Mahdi Tavakol <mahditavakol90@gmail.com>
2026-02-16 12:25:11 +0000

    Reinstated the constrain_v_lambdas

--- src/CONSTANT_PH/fix_nh_constant_pH.cpp
+++ src/CONSTANT_PH/fix_nh_constant_pH.cpp
@@ -163,8 +163,8 @@ void FixNHConstantPH::nve_v()
     v_lambda_buff += dt * a_lambda_buff;
   }
 
-  //if (lambda_integration_flags & CONSTRAIN)
-    //constrain_v_lambdas();
+  if (lambda_integration_flags & CONSTRAIN)
+    constrain_v_lambdas();
 
   // Returning the modified parameters to the fix_constant_pH.
   fix_constant_pH->reset_params(pH_state);
